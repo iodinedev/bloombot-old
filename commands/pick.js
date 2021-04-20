89bdc217b9bf95c27d2f3bb3b7b9adb18fe73aa1
zmontgo <31627113+zmontgo@users.noreply.github.com>
2021-04-20 10:02:32 -0500

    Add to do

--- commands/pick.js
+++ commands/pick.js
@@ -1,11 +1,40 @@
+const config = require('../config.json');
+const Discord = require('discord.js');
+
 module.exports.execute = async (client, message) => {
-  return await message.channel.send('Hey there!');
+  let member = message.guild.roles.get(config.roles.challengers).members.cache.random().user;
+  
+  var usr = MeditationModel.findOne({usr: message.author.id});
+  var all_time = 0;
+
+  if (usr) {
+    all_time = usr.all_time;
+  }
+
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth() + 1;
+  var day = dateObj.getUTCDate();
+  var year = dateObj.getUTCFullYear();
+
+  const announceEmbed = new Discord.MessageEmbed()
+    .setColor(config.colors.embedColor)
+    .setTitle(':tada: This month\'s meditation challenger in the spotlight is... :tada:')
+    .addField(
+      `**Monthly hall-of-fame member**`,
+      `**${member.mention}** is our server member of the month, with a meditation time of **${all_time}** minutes!\nYou're doing great, keep at it!`
+    )
+    .setFooter(`${day}.${month}.${year}`);
+
+  var channel = client.channels.cache.get(config.channels.announce_channel);
+
+  return await channel.send(announceEmbed);
 };
 
 module.exports.config = {
-  name: 'hello',
+  name: 'pick',
   aliases: ['hey', 'greetings'],
   module: 'Utility',
   description: 'Says hello. Use to test if bot is online.',
   usage: ['hello'],
+  admin: true
 };
