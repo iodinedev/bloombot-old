9fe12073b51ba685501674ca243ee2ba48d3ec9a
Zachary M <31627113+zmontgo@users.noreply.github.com>
2021-04-21 10:38:05 -0500

    Update meditate.js

--- commands/meditate.js
+++ commands/meditate.js
@@ -51,46 +51,32 @@ function begin(message, voiceChannel, link) {
   }
 }
 
-async function stop(client, date, meditation, catchUp = false) {
+async function stop(client, date, meditation, difference, catchUp = false) {
 	let userToStop = await client.users.fetch(meditation.usr);
 	let color, description;
+	var time = meditation.time;
 
 	if (catchUp) {
-		color = '#FF4500';
 		description = `Whoops! Sorry for being late, I was probably down for maintenance. 😅
-		Anyway, you have finished your **${meditation.time}** minutes of meditation, with an additional `;
+		Anyway, you have finished your **${meditation.time}** minutes of meditation, with an additional time of **${difference}**. I've added it to your total.`;
+		time = time + difference;
 	} else {
-		color = '#FFCC00';
-		description = `Hello! I'm sorry to barge in like that. 🤐
-		I just wanted to remind you to **${reminder.whatToRemind}**. Off I go. 😄`;
+		description = `Hello! Your **${meditation.time}** minutes of meditation are done! I've added it to your total.`
 	}
 
-	const remindMessage = new Discord.MessageEmbed()
-		.setColor(color)
-		.setTitle(`${config.emotes.reminders} Reminder ${config.emotes.reminders}`)
+	const stopMessage = new Discord.MessageEmbed()
+		.setColor(config.embed_color)
+		.setTitle(`${config.emotes.meditation} Meditation Time Done ${config.emotes.meditation}`)
 		.setDescription(description);
 
-	userToRemind.send(remindMessage);
+	userToRemind.send(stopMessage);
 
-	if (!reminder.recurring) {
-		try {
-			await Reminder.deleteOne({
-				id: reminder._id
-			});
-		} catch(err) {
-			console.error('Reminder MongoDB error: ', err);
-		}
-	} else {
-		let [amountToAdd, whatToAdd] = reminder.howOftenToRemind.split(' ');
-		amountToAdd = parseInt(amountToAdd);
-		try {
-			await Reminder.updateOne(
-				{ id: reminder._id },
-				{ $set: { whenToRemind: remindUtils.addToDate(date, amountToAdd, whatToAdd) } }
-			);
-		} catch(err) {
-			console.error('Reminder MongoDB error: ', err);
-		}
+	try {
+		await Current.deleteOne({
+			id: reminder._id
+		});
+	} catch(err) {
+		console.error('Meditation MongoDB error: ', err);
 	}
 }
 
@@ -139,8 +125,8 @@ module.exports.catchUp = catchUp;
 
 module.exports.config = {
   name: 'meditate',
-  aliases: ['hey', 'greetings'],
-  module: 'Utility',
-  description: 'Says hello. Use to test if bot is online.',
-  usage: ['hello'],
+  aliases: [],
+  module: 'Meditation',
+  description: 'Keeps track of your meditation time for you. Join a voice channel and it will join and play a gong sound to mark the beginning and end.\nIt will leave in between the gong sounds, but you must stay in the voice channel or it will stop it early.',
+  usage: ['meditate <time in minutes>'],
 };
