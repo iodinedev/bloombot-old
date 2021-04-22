b0a373fd57268450d2068533c0a5a6cff0e244a5
Zachary M <31627113+zmontgo@users.noreply.github.com>
2021-04-22 13:27:37 -0500

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,3 +1,32 @@
+const meditation = require('../commands/meditate.js');
+const Current = require('../databaseFiles/connect').Current;
+const config = require('../config.json');
 
+module.exports = async (client, oldMember, newMember) => {
+  const currentDate = new Date();
 
-stop(client, meditation, difference)
+	try {
+		const meditation = await Current.findOne({
+      usr: newMember.id
+    });
+
+		if (meditation) {
+      let difference;
+      difference = currentDate - meditation.whenToStop;
+
+      difference = difference.getMinutes();
+
+      await Current.updateOne(
+        { usr: newMember.id },
+        { $set: {
+            time: difference
+          }
+        }
+      );
+
+      meditation.stop(client, meditation, difference);
+		}
+	} catch(err) {
+		console.error('Meditation MongoDB error: ', err);
+	}
+}
