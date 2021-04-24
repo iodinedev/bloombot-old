0db0e93a0c32393fbc9f2bcb72a68649e74a3ecd
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-04-24 12:59:34 -0500

    Remove prefix from remove

--- commands/remove.js
+++ commands/remove.js
@@ -2,15 +2,14 @@ const Meditations = require('../databaseFiles/connect').Meditations;
 const Prefixes = require('../databaseFiles/connect').Prefixes;
 const { ObjectId } = require('mongodb');
 
-let prefix;
-try {
-  prefix = await Prefixes.findOne({'guild': message.guild.id});
-  prefix = prefix.prefix;
-} catch {
-  prefix = '.';
-}
-
 module.exports.execute = async (client, message, args) => {
+  let prefix;
+  try {
+    prefix = await Prefixes.findOne({'guild': message.guild.id});
+    prefix = prefix.prefix;
+  } catch {
+    prefix = '.';
+  }
 
   if (!args[0]) return await message.channel.send(`:x: You must include a meditation ID to remove. Use \`${prefix}rank\` to see your recent meditations' IDs.`)
   const id = ObjectId(args[0]);
@@ -33,6 +32,6 @@ module.exports.config = {
   name: 'remove',
   aliases: [],
   module: 'Meditation',
-  description: `Deletes a meditation session by ID. Use \`${prefix}rank\` to see your recent meditations as well as their IDs.`,
+  description: `Deletes a meditation session by ID. Use the \`rank\` command to see your recent meditations as well as their IDs.`,
   usage: ['remove <meditation ID>'],
 };
