34acec945122dc0d434e9bdd608c1e2d477f4efd
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-06-19 14:44:53 -0500

    Fix issue

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -2,7 +2,7 @@ const reportActions = require('../eventActions/reportAction');
 
 module.exports = async (client, reaction, user) => {
 	// When we receive a reaction we check if the reaction is partial or not, and return because this event will be fired by raw
-	if (reaction && reaction.partial) {
+	if (!reaction || reaction.partial) {
 		return;
 	}
 
