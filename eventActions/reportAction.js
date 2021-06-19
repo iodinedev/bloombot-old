7ded4583d666a61afab5e3aabe5f977eaad6e721
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-06-19 14:52:31 -0500

    Make not anonymous

--- eventActions/reportAction.js
+++ eventActions/reportAction.js
@@ -8,14 +8,13 @@ class reportCheckAction {
 
     try {
       if (reaction._emoji && reaction._emoji.id === config.emotes.report) {
-        console.log(user)
         await reaction.users.remove(user.id);
         var channel = client.channels.cache.get(message.channel.id);
         let starBoardMessage = new Discord.MessageEmbed()
           .setColor(config.colors.embedColor)
           .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL())
           .setDescription(message.content)
-          .setFooter(`Reported in #${channel.name}`)
+          .setFooter(`Reported in #${channel.name} by ${user.username}`)
           .setTimestamp(message.createdAt);
         client.channels.cache.get(config.channels.reportchannel).send(starBoardMessage)
         .then(() => {
