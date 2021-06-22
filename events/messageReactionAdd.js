7d4da6c99f1eb566be922b2926f2fbb5997bb883
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-06-22 06:46:39 -0500

    Add starboard

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,4 +1,5 @@
 const reportActions = require('../eventActions/reportAction');
+const starboardActions = require('../eventActions/starboardActions');
 
 module.exports = async (client, reaction, user) => {
 	// When we receive a reaction we check if the reaction is partial or not, and return because this event will be fired by raw
@@ -8,4 +9,6 @@ module.exports = async (client, reaction, user) => {
 
 	// Check if user is reporting a message
 	reportActions.checkReport(client, user, reaction);
+  // Check if message should be added to starboard or if starboard message should be updated
+	starboardActions.addStar(client, user, reaction);
 };
