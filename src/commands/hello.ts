1304dde2bb4c7443e76632fc2527f5c4278cada3
Zachary M <31627113+zmontgo@users.noreply.github.com>
2021-09-20 11:45:17 -0500

    Test new workflow

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,5 +1,5 @@
 export const execute = async (client, message) => {
-  return await message.channel.send('Hey there!');
+  return await message.channel.send('Hello there!');
 };
 
 export const architecture = {
