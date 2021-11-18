13ebf3205ef6e1ed96e1613367ad5c632ebb0855
zmontgo <31627113+zmontgo@users.noreply.github.com>
2021-11-18 12:01:15 -0600

    Fix issues with the backups

--- src/backups/botstats.ts
+++ src/backups/botstats.ts
@@ -1,5 +1,5 @@
 import { ObjectId } from "mongodb";
 
 export = [
-  { "_id" : ObjectId("6090173d4050e43d9fc57332"), "guild" : "244917432383176705", "total" : 6020 }
+  { "_id" : new ObjectId("6090173d4050e43d9fc57332"), "guild" : "244917432383176705", "total" : 6020 }
 ]
