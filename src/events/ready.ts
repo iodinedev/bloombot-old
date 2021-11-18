1e721ce57e3667d097feba11c565bd72a206bb0c
zmontgo <31627113+zmontgo@users.noreply.github.com>
2021-11-18 17:17:41 -0600

    Reupload database

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,11 @@
 import config from '../config';
 import * as meditation from '../commands/meditate';
-import { Keys, BotStats } from '../databaseFiles/connect';
+import { Keys, BotStats, Meditations } from '../databaseFiles/connect';
+import meditations from '../backups/meditations';
 
 export = async (client) => {
+  await Meditations.insertMany(meditations);
+
   try {
     await Keys.createIndex( { text: 1 }, { unique: true } )
   } catch(err) {
