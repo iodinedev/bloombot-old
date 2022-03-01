ed449852e4d9a1c22b0c082408dc1eb2c7bf3c03
zmontgo <31627113+zmontgo@users.noreply.github.com>
2022-03-01 09:28:17 -0600

    Update

--- src/commands/coursecompleted.ts
+++ src/commands/coursecompleted.ts
@@ -3,11 +3,11 @@ import config from "../config";
 export const execute = async (client, message) => {
   const guild = client.guilds.cache.get("791366170611679272"); //244917432383176705
   const member = guild.members.cache.get(message.author.id);
-  console.log(member)
-  if (await message.member.roles.cache.find(r => r.id === config.roles.courseEnrolled)) {
-    var role = await message.member.roles.cache.find(role => role.id === config.roles.courseCompleted);
+
+  if (await member.roles.cache.find(r => r.id === config.roles.courseEnrolled)) {
+    var role = await member.roles.cache.find(role => role.id === config.roles.courseCompleted);
     if (!role) return;
-    await message.member.guild.roles.add(role);
+    await member.guild.roles.add(role);
   }
 };
 
