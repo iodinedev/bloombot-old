8ed2e09809a7ff4f8c51339fb4431a919804853f
zmontgo <31627113+zmontgo@users.noreply.github.com>
2022-03-17 14:43:28 -0500

    Further testing

--- src/transition.ts
+++ src/transition.ts
@@ -42,16 +42,9 @@ async function transition() {
     guild: string,
   }[] = [];
     
-  const data5 = await Meditations.find().toArray();
-  for await (const meditatation of data5) {
-    meditations.push({
-      usr: meditatation.usr,
-      date: `${meditatation.date}`,
-      time: meditatation.time,
-      guild: meditatation.guild
-    })
-  }
-  await prisma.meditations.createMany({data: meditations});
+  const data5 = await Meditations.find({}, { projection: { _id:0 }}).toArray();
+  console.log(data5)
+  //await prisma.meditations.createMany({data: meditations});
   /*const data7: any = await ServerSetup.find().toArray();
   await prisma.tags.createMany({data: data7});
   const data8: any = await Stars.find().toArray();
