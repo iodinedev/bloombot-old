bf75ce33cd7f67441ff1e49d290fa0a73daef51b
Zachary Montgomery <zachmontgo@iodinedev.com>
2022-07-01 23:50:16 -0500

    Even more more testing

--- src/commands/pick.ts
+++ src/commands/pick.ts
@@ -37,8 +37,8 @@ export const execute = async (client, message) => {
     }[] = userdata.latest;
 
     console.log(latest)
-    console.log(new Date(parseInt(latest[0].date)))
-    console.log(new Date(parseInt(latest[1].date)))
+    console.log(new Date(parseInt(latest[0].date)) >= now)
+    console.log(new Date(parseInt(latest[1].date)) >= now)
 
     if (
       latest.length > 1 &&
